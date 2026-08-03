db80b86b0b23adfedd05b20d132589f11c41f240
Kohly Akpet <koakpet@yahoo.co.uk>
2026-08-03 07:37:52 +0100

    ...

--- src/components/layout/Logo.jsx
+++ src/components/layout/Logo.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="flex items-center gap-1">
-      <div className="overflow-hidden h-15 w-15 rounded-full">
+      <div className="overflow-hidden h-12 w-12 md:h-15 md:w-15 rounded-full">
         <Image
           src="/logo.jpg"
           width={120}
@@ -13,7 +13,7 @@ export default function Logo() {
         />
       </div>
       <div>
-        <p className="text-sm font-semibold text-slate-900">
+        <p className="text-sm md:text-sm font-semibold text-slate-900">
           Court of Appeal Staff
         </p>
         <p className="text-xs text-slate-500">
